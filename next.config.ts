3b40b0f03e99d46c09e180e77107971df53d946a
ultra <77009494+0x-ultra@users.noreply.github.com>
2025-04-06 12:42:44 +0700

    how about now

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["three"],
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,4 @@
+/** @type {import('postcss').Config} */
 export default {
   plugins: {
     tailwindcss: {},
